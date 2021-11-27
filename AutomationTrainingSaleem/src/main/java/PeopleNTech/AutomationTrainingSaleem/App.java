27cb22302dc09fd169b5550fcd7f408ec9310b80
eRisheXpress <eRisheXpress@LAPTOP-MFKDHCQD.myfiosgateway.com>
2021-11-27 13:09:37 -0500

    First time checking

---
+++ AutomationTrainingSaleem/src/main/java/PeopleNTech/AutomationTrainingSaleem/App.java
@@ -0,0 +1,13 @@
+package PeopleNTech.AutomationTrainingSaleem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
